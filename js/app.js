cf418ab4687f441698f30b29a0a1d3b400210ce9
gut7y <deswebproy@gmail.com>
2022-05-09 12:25:11 -0500

    extraer datos del html

---
+++ js/app.js
@@ -0,0 +1,37 @@
+// Variables 
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito')
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners (){
+    //cuando agregas un curso presionando "agregar al carrito"
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+
+//Funciones 
+function agregarCurso(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito') ) {
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+    }
+
+    
+}
+
+//Lee el contendio del HTML 
+function leerDatosCurso(curso) {
+    console.log(curso);
+
+    //Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+}
